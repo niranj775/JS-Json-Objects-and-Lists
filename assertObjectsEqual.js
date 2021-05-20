156aaa6eebe41b3a0ce06f1b770281a77be6006c
E NIRANJANKUMAR <niranj775@gmail.com>
2021-05-20 20:59:39 +0530

    first commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,25 @@
+var expected = {foo: 5, bar: 6};
+var actual = {foo: 5, bar: 6};
+function assertObjectsEqual(actual, expected, testName){
+
+}
+
+/*
+Write an “assertObjectsEqual” function from scratch.
+Assume that the objects in question contain only scalar values (i.e., simple values like strings or numbers).
+It is OK to use JSON.stringify().
+Note: The examples below represent different use cases for the same test. In practice, you should never have multiple tests with the same name.
+Success Case:
+Input:
+var expected = {foo: 5, bar: 6};
+var actual = {foo: 5, bar: 6}
+assertObjectsEqual(actual, expected, ‘detects that two objects are equal’);
+Output:
+Passed
+Failure Case:
+Input:var expected = {foo: 6, bar: 5};
+var actual = {foo: 5, bar: 6}
+assertObjectsEqual(actual, expected, ‘detects that two objects are equal’);
+Output:
+FAILED [my test] Expected {“foo”:6,”bar”:5}, but got {“foo”:5,”bar”:6}
+*/
---
+++ test.js
@@ -0,0 +1,9 @@
+    
+    console.log(`typeof(1): ${typeof(1)}`);
+    console.log(`typeof(1.1): ${typeof(1.1)}`);
+    console.log(`typeof("1.1"): ${typeof("1.1")}`);
+    console.log(`typeof(true): ${typeof(true)}`);
+    console.log(`typeof(null): ${typeof(null)}`);
+    console.log(`typeof(undefined): ${typeof(undefined)}`);
+    console.log(`typeof([]): ${typeof([])}`);
+    console.log(`typeof({}): ${typeof({})}`);
